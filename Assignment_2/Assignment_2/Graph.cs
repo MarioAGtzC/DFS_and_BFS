e8827072ee132ae29a21630a79af7ea52d0d64e5
Mario <32181249+MarioAGtzC@users.noreply.github.com>
2019-10-17 23:07:02 -0600

    Add files via upload

---
+++ Assignment_2/Assignment_2/Graph.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment_2
+{
+    class Graph
+    {
+        private char _maxValue;
+        private bool _directed;
+        LinkedList<char>[] adjacency;
+
+        public Graph(char maxValue, bool directed)
+        {
+            adjacency = new LinkedList<char>[maxValue];
+
+            for (int i = 0; i < adjacency.Length; i++)
+            {
+                adjacency[i] = new LinkedList<char>();
+            }
+
+            _maxValue = maxValue;
+            _directed = directed;
+        }
+
+        public void AddEdge(char vertex, char end)
+        {
+            adjacency[vertex].AddLast(end);
+
+            if (!_directed)
+            {
+                adjacency[end].AddLast(vertex);
+            }
+        }
+
+        public void DFS(char vertex)
+        {
+            // Mark all the vertices as not visited
+            bool[] visited = new bool[_maxValue];
+
+            for (int i = 0; i < _maxValue; i++)
+                visited[i] = false;
+
+            // Call the recursive helper function to print DFS traversal
+            DFSUtil(vertex, visited);
+        }
+
+        private void DFSUtil(char vertex, bool[] visited)
+        {
+            visited[vertex] = true;
+            Console.Write(vertex + " ");
+
+            LinkedList<char> list = adjacency[vertex];
+
+            foreach (var val in list)
+            {
+                if (!visited[val])
+                    DFSUtil(val, visited);
+            }
+        }
+
+        public void BFS(char s)
+        {
+            bool[] visited = new bool[_maxValue];
+            for (int i = 0; i < _maxValue; i++)
+                visited[i] = false;
+
+            LinkedList<char> queue = new LinkedList<char>();
+
+            visited[s] = true;
+            queue.AddLast(s);
+
+            while (queue.Any())
+            {
+                s = queue.First();
+                Console.Write(s + " ");
+                queue.RemoveFirst();
+
+                LinkedList<char> list = adjacency[s];
+
+                foreach (var val in list)
+                {
+                    if (!visited[val])
+                    {
+                        visited[val] = true;
+                        queue.AddLast(val);
+                    }
+                }
+            }
+        }
+
+    }
+}
+
+
---
+++ Assignment_2/Assignment_2/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Collections.Generic;
+
+namespace Assignment_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Graph graph = new Graph('S', true);
+            graph.AddEdge('A', 'B');
+            graph.AddEdge('A', 'C');
+            graph.AddEdge('A', 'D');
+            graph.AddEdge('B', 'E');
+            graph.AddEdge('B', 'F');
+            graph.AddEdge('B', 'G');
+            graph.AddEdge('C', 'G');
+            graph.AddEdge('D', 'H');
+            graph.AddEdge('D', 'I');
+            graph.AddEdge('E', 'J');
+            graph.AddEdge('E', 'K');
+            graph.AddEdge('E', 'L');
+            graph.AddEdge('F', 'A');
+            graph.AddEdge('F', 'L');
+            graph.AddEdge('G', 'H');
+            graph.AddEdge('G', 'M');
+            graph.AddEdge('G', 'N');
+            graph.AddEdge('H', 'A');
+            graph.AddEdge('H', 'O');
+            graph.AddEdge('H', 'P');
+            graph.AddEdge('I', 'P');
+            graph.AddEdge('I', 'R');
+
+            Console.Write("DFS and BFS from A:\n");
+            Console.Write("DFS: ");
+            graph.DFS('A');
+            Console.Write("\nBFS: ");
+            graph.BFS('A');
+            Console.ReadLine();
+        }
+    }
+}
